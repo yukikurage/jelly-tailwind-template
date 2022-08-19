2d40c5c7e765619a8b9e6607f8e4ce7970984c50
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-08-19 12:14:47 +0900

    make Web app

--- src/Main.purs
+++ src/Main.purs
@@ -2,32 +2,16 @@ module Main where
 
 import Prelude
 
-import Data.Array (catMaybes, fold, range)
-import Data.Functor (mapFlipped)
-import Data.Maybe (Maybe(..))
+import Control.Monad.Reader (runReader)
+import Data.Array (range)
 import Data.String (joinWith)
-import Data.Tuple.Nested (type (/\), (/\))
 import Effect (Effect)
+import FizzBuzz (fizzbuzz)
 import Jelly (Component, ch, el, launchApp, text)
 
 main :: Effect Unit
 main = launchApp root unit
 
 root :: Component Unit
-root = el "pre" $ ch $ text $ pure $ joinWith "\n" $ map fizzBuzz $ range 1 100
-
-fizzBuzzRules :: Array (Int /\ String)
-fizzBuzzRules =
-  [ 3 /\ "Fizz"
-  , 5 /\ "Buzz"
-  ]
-
-fizzBuzz :: Int -> String
-fizzBuzz i =
-  let
-    matches = mapFlipped fizzBuzzRules \(divisor /\ word) ->
-      if i `mod` divisor == 0 then Just word else Nothing
-  in
-    case catMaybes matches of
-      [] -> show i
-      words -> fold words
+root = el "pre" do
+  ch $ text $ pure $ joinWith "\n" $ map (runReader fizzbuzz) $ range 1 100
